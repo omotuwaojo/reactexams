6fbb69e2bcc20e3d786e0fbb895e52e36472c1e1
Omotuwa Ojo <75334653+omotuwaojo@users.noreply.github.com>
2025-01-08 04:22:09 -0800

    routing

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://api.razzakfashion.com",
+        target: "https://api.razzakfashion.com/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
