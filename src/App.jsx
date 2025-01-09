1294df8a969c0595408de0a15c322ebc852094b8
Omotuwa Ojo <75334653+omotuwaojo@users.noreply.github.com>
2025-01-09 14:59:54 -0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
     <Router >
       <div className="p-4">
         <h1 className="text-2xl font-bold mb-4 text-center">Multi-Feature React App</h1>
-        <nav className="mb-4 flex justify-center space-x-4">
+        <nav className="mb-4 flex justify-center p-4 space-x-4">
           <Link to="/" className="bg-blue-200 text-white px-4 py-2 rounded">Custom Table</Link>
           <Link to="/tshirt-designer" className="bg-green-500 text-white px-4 py-2 rounded">T-Shirt Designer</Link>
         </nav>
